36f45f784f575f771358027eff16a6a06f07e964
Rahul jain <rahul.jain@w3villa.com>
2022-08-02 16:29:12 +0530

    adding api and stock table

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,15 @@
 const { environment } = require('@rails/webpacker')
+const { default: Popper } = require('popper.js')
+
+const webpack = require('webpack')
+
+environment.plugins.append("Provide", new webpack.ProvidePlugin({
+
+    $: 'jquery',
+    jquery: 'jquery',
+    Popper: ['popper.js', 'default']
+
+}))
+
 
 module.exports = environment
